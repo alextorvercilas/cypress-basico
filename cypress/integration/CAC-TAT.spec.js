06b11f33b324e18bc96b5bc02d0d80b6479d06b0
Alexander Torvercilas de Melo <alexhg2000@gmail.com>
2024-10-01 15:15:27 -0300

    commit
    1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -11,5 +11,6 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
       cy.visit('./src/index.html')
       cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+      //****// */
     })
   })
