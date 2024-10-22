79d45e8cdadeeab08c34b8df416e6aad09fccf6d
Alexander Torvercilas de Melo <alexhg2000@gmail.com>
2024-10-22 15:23:53 -0300

    Ultimo commit do curso
    Comitando ultimas alterações

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -18,7 +18,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
       //****// */
     })
-    it.only('preenche os campos obrigatórios e envia o formulário',function()
+    it('preenche os campos obrigatórios e envia o formulário',function()
     {
       const longText = 'Estou ficando cansado, da tal tecnologia, Só se fala por e-mail, Mensagem curta e fria, Twitter e Facebook, Antes que eu caduque, Vou dizer tudo em poesia'
 
@@ -169,5 +169,51 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .click()
         cy.contains('Talking About Testing').should('be.visible')
       })
-
+      it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+          cy.get('.success')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Mensagem enviada com sucesso.')
+            .invoke('hide')
+            .should('not.be.visible')
+          cy.get('.error')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Valide os campos obrigatórios!')
+            .invoke('hide')
+            .should('not.be.visible')
+      })
+      it('preenche a area de texto usando o comando invoke',function(){
+        const longText = Cypress._.repeat('0123456789 ', 20)
+        cy.get('#open-text-area')
+         .invoke('val',longText)
+         .should('have.value', longText)
+      })
+      it('faz uma requisição HTTP',function() {
+         cy.request ('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+          .should(function(response){
+           const {status, statusText, body} = response
+           expect(status).to.equal(200)
+           expect(statusText).to.equal('OK')
+           expect(body).to.include('CAC TAT')
+          })
+      })
+      it('encontre um gato na requisicao',function() {
+        cy.request ('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+         .should(function(response){
+          const {body} = response
+          expect(body).to.include('cat')
+         })
+     })
+     it.only('encontre um gato na aplicacao',function() {
+      cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
+      cy.get('#title')
+        .invoke('text','CAT TAT')
+      cy.get ('#subtitle')
+        .invoke('text','Eu ❤️ Gatos')  
+   })
   })
